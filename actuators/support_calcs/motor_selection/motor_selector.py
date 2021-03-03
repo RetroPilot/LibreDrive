255a9639d518ba32f4d6d4ae3ac290ad2a2276cc
amzoo <armando.m.armijo@gmail.com>
2021-03-03 10:37:01 -0800

    Added pedal calcs to motor_selector

--- actuators/support_calcs/motor_selection/motor_selector.py
+++ actuators/support_calcs/motor_selection/motor_selector.py
@@ -16,6 +16,28 @@ steering = {
     }
 }
 
+pedal = {
+    'inputs': {
+        'forces': ['pedal_force_N', 'peak_actuation_time_s', 'travel_distance_mm'],
+        'dimensions': ['driven_pulley_dia_mm', 'gear_ratio', 'gearbox_ratio'],
+        'electrical': ['voltage_V']
+    },
+    'outputs': {
+        'forces': ['motor_torque_Nm', 'motor_velocity_RPM'],
+        'electrical': ['estimated_power_W', 'estimated_current_A']
+    }
+}
+
+pi = 3.141592653589793
+
+def assert_data_format(data, data_format):
+    for key in data_format.keys():
+        assert key in data, "Missing '{}' in input json".format(key)
+        for prop_type in data_format[key].keys():
+            assert prop_type in data[key], "Missing '{}:{}' in input json".format(key, prop_type)
+            for prop in data_format[key][prop_type]:
+                assert prop in data[key][prop_type], "Missing '{}:{}:{}' in input json".format(key, prop_type, prop)
+
 
 def main(args):
     fname = args.input_json
@@ -29,14 +51,9 @@ def main(args):
 
         assert data['type'] in types, "'type' may only be {}".format(types)
 
-        if data['type'] == "steering":
+        if data['type'] == 'steering':
             # Data assertions
-            for key in steering.keys():
-                assert key in data, "Missing '{}' in input json".format(key)
-                for prop_type in steering[key].keys():
-                    assert prop_type in data[key], "Missing '{}:{}' in input json".format(key, prop_type)
-                    for prop in steering[key][prop_type]:
-                        assert prop in data[key][prop_type], "Missing '{}:{}:{}' in input json".format(key, prop_type, prop)
+            assert_data_format(data, steering)
 
             # inputs
             steering_torque_Nm = data['inputs']['forces']['steering_torque_Nm']
@@ -62,8 +79,33 @@ def main(args):
             data['outputs']['electrical']['estimated_current_A'] = round(estimated_current_A, precision)
 
         elif data['type'] == "pedal":
-            print("Pedal calculations coming soon")
-            pass
+            # Data assertions
+            assert_data_format(data, pedal)
+
+            # inputs
+            pedal_force_N = data['inputs']['forces']['pedal_force_N']
+            peak_actuation_time_s = data['inputs']['forces']['peak_actuation_time_s']
+            travel_distance_mm = data['inputs']['forces']['travel_distance_mm']
+            driven_pulley_dia_mm = data['inputs']['dimensions']['driven_pulley_dia_mm']
+            gear_ratio = data['inputs']['dimensions']['gear_ratio']
+            gearbox_ratio = data['inputs']['dimensions']['gearbox_ratio']
+            voltage_V = data['inputs']['electrical']['voltage_V']
+
+            # outputs
+            precision = args.precision
+            pulley_velocity_rps = travel_distance_mm/peak_actuation_time_s / (pi*driven_pulley_dia_mm)
+            pulley_torque_Nm = pedal_force_N * (driven_pulley_dia_mm/2)/1000
+            gearbox_torque_Nm = pulley_torque_Nm / gear_ratio
+            motor_torque_Nm = gearbox_torque_Nm / gearbox_ratio
+            motor_velocity_RPM = pulley_velocity_rps * gear_ratio * gearbox_ratio * 60
+            estimated_power_W = motor_torque_Nm * motor_velocity_RPM / 9.5488
+            estimated_current_A = estimated_power_W / voltage_V
+
+            data['outputs']['forces']['motor_torque_Nm'] = round(motor_torque_Nm, precision)
+            data['outputs']['forces']['motor_velocity_RPM'] = round(motor_velocity_RPM, precision)
+
+            data['outputs']['electrical']['estimated_power_W'] = round(estimated_power_W, precision)
+            data['outputs']['electrical']['estimated_current_A'] = round(estimated_current_A, precision)
 
         output_data.append(data)
 
